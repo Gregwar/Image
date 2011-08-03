3df20e12c00eba2e21acf3d020f586d9bda53881
Gregwar <g.passault@gmail.com>
2011-08-03 20:59:03 +0200

    Moved image files in the demo directory to a img/ subdirectory

--- demo/basic.php
+++ demo/basic.php
@@ -3,7 +3,7 @@ require_once('../lib/Gregwar/Image.php');
 
 use Gregwar\Image;
 
-Image::open('test.png')
+Image::open('img/test.png')
     ->resize(100, 100)
     ->negate()
     ->save('out.jpg');
