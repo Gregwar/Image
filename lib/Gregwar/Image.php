0434435d2ed2960ca97e328d2bbcda0a1cd9584b
Grégoire Passault <g.passault@alienor.net>
2011-08-05 09:39:12 +0200

    Added the possibility of getting the image contents using ->get() instead of ->save()

--- lib/Gregwar/Image.php
+++ lib/Gregwar/Image.php
@@ -667,6 +667,9 @@ class Image
 
         $success = false;
 
+        if (null == $file)
+            ob_start();
+
         if ($type == 'jpeg') 
             $success = imagejpeg($this->gd, $file, $quality);
 
@@ -679,7 +682,15 @@ class Image
         if (!$success)
             return false;
 
-        return $file;
+        return (null === $file ? ob_get_clean() : $file);
+    }
+
+    /**
+     * Get the contents of the image
+     */
+    public function get($type = 'jpeg', $quality = 80)
+    {
+        return $this->save(null, $type, $quality);
     }
 
     /* Image API */
