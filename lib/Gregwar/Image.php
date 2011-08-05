ce96a7c26708217fcc69af4858ba28bd28a36d3c
Grégoire Passault <g.passault@alienor.net>
2011-08-05 09:45:27 +0200

    Added ->correct() telling if the file is a valid image (using exif)

--- lib/Gregwar/Image.php
+++ lib/Gregwar/Image.php
@@ -112,6 +112,14 @@ class Image
     }
 
     /**
+     * Tells if the image is correct
+     */
+    public function correct()
+    {
+        return (false !== @exif_imagetype($this->file));
+    }
+
+    /**
      * Guess the file type
      */
     protected function guessType()
