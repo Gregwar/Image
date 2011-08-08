1bdf2f89a55d5dc6f95fbd54f8dd4dcc808f2b80
Grégoire Passault <g.passault@alienor.net>
2011-08-08 17:17:47 +0200

    Addes support for "rgb(255, 0, 0)" like colors

--- demo/write.php
+++ demo/write.php
@@ -4,7 +4,7 @@ require_once('../lib/Gregwar/Image.php');
 use Gregwar\Image;
 
 Image::create(300, 300)
-    ->fill('0xfaa')
+    ->fill('rgb(255, 150, 150)')
     ->circle(150, 150, 200, 'red', true)
     ->write('./fonts/CaviarDreams.ttf', "Hello world!", 150, 150, 20, 0, 'white', 'center')
     ->save('out.jpg', 'jpeg', 95);
--- lib/Gregwar/ImageColor.php
+++ lib/Gregwar/ImageColor.php
@@ -38,9 +38,11 @@ class ImageColor
         if (isset(self::$colors[$color]))
             return self::$colors[$color];
 
-        // Color string (ex: "ff0000", "#ff0000" or "0xfff")
         if (is_string($color)) {
-            if (preg_match('/^(#|0x|)([0-9a-f]{3,6})/i', $color, $matches)) {
+            $color_string = str_replace(' ', '', $color);
+
+            // Color string (ex: "ff0000", "#ff0000" or "0xfff")
+            if (preg_match('/^(#|0x|)([0-9a-f]{3,6})/i', $color_string, $matches)) {
                 $col = $matches[2];
 
                 if (strlen($col) == 6)
@@ -51,6 +53,16 @@ class ImageColor
                     for ($i=0; $i<3; $i++)
                         $r.= $col[$i].$col[$i];
                     return hexdec($r);
+                }
+            }
+            
+            // Colors like "rgb(255, 0, 0)"
+            if (preg_match('/^rgb\(([0-9]+),([0-9]+),([0-9]+)\)/i', $color_string, $matches)) {
+                $r = $matches[1];
+                $g = $matches[2];
+                $b = $matches[3];
+                if ($r>=0 && $r<=0xff && $g>=0 && $g<=0xff && $b>=0 && $b<=0xff) {
+                    return ($r << 16) | ($g << 8) | ($b);
                 }
             }
         }
