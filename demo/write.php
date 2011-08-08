78c7730c467c3aac87eb1a66e7ccc8bd7e479165
Grégoire Passault <g.passault@alienor.net>
2011-08-08 17:11:42 +0200

    Added support for "#ff0000" or "#0x00ff00" strings for color

--- demo/write.php
+++ demo/write.php
@@ -4,7 +4,7 @@ require_once('../lib/Gregwar/Image.php');
 use Gregwar\Image;
 
 Image::create(300, 300)
-    ->fill(0xffaaaa)
-    ->circle(150, 150, 200, 0xff3333, true)
+    ->fill('0xfaa')
+    ->circle(150, 150, 200, 'red', true)
     ->write('./fonts/CaviarDreams.ttf', "Hello world!", 150, 150, 20, 0, 'white', 'center')
     ->save('out.jpg', 'jpeg', 95);
--- lib/Gregwar/ImageColor.php
+++ lib/Gregwar/ImageColor.php
@@ -30,11 +30,30 @@ class ImageColor
 
     public static function parse($color)
     {
-        if (is_numeric($color))
+        // Direct color representation (ex: 0xff0000)
+        if (!is_string($color) && is_numeric($color))
             return $color;
 
+        // Color name (ex: "red")
         if (isset(self::$colors[$color]))
             return self::$colors[$color];
+
+        // Color string (ex: "ff0000", "#ff0000" or "0xfff")
+        if (is_string($color)) {
+            if (preg_match('/^(#|0x|)([0-9a-f]{3,6})/i', $color, $matches)) {
+                $col = $matches[2];
+
+                if (strlen($col) == 6)
+                    return hexdec($col);
+
+                if (strlen($col) == 3) {
+                    $r = '';
+                    for ($i=0; $i<3; $i++)
+                        $r.= $col[$i].$col[$i];
+                    return hexdec($r);
+                }
+            }
+        }
 
         throw new \InvalidArgumentException('Invalid color: '.$color);
     }
