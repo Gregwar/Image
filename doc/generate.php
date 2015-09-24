0f06287a1a5af75dcb4f490cc7217a376eac03d7
Sullivan SENECHAL <soullivaneuh@gmail.com>
2015-09-24 03:35:50 -0400

    Applied fixes from StyleCI

--- doc/generate.php
+++ doc/generate.php
@@ -20,7 +20,7 @@ Image::open('mona.jpg')
     ->forceResize(250, 250)
     ->save('forceResize.jpg');
 
-// cropResize() preserves scale just like resize() but will 
+// cropResize() preserves scale just like resize() but will
 // trim the whitespace (if any) in the resulting image
 Image::open('mona.jpg')
     ->cropResize(250, 250)
