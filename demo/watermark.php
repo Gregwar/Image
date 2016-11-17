8fab753f47982067962db44ac98f4ec1cf43c4e3
Gregwar <g.passault@gmail.com>
2016-11-17 10:10:34 +0100

    Comments

--- demo/watermark.php
+++ demo/watermark.php
@@ -4,8 +4,13 @@ require_once '../autoload.php';
 
 use Gregwar\Image\Image;
 
+// Opening mona.jpg
 $img = Image::open('img/mona.jpg');
+
+// Opening vinci.png
 $watermark = Image::open('img/vinci.png');
+
+// Mergine vinci text into mona in the top-right corner
 $img->merge($watermark, $img->width()-$watermark->width(),
     $img->height()-$watermark->height())
     ->save('out.jpg', 'jpg');
