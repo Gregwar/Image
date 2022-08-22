a987be8d11c022423c7f2ed423b297dc48e4a712
Rotzbua <Rotzbua@users.noreply.github.com>
2022-08-22 18:36:41 +0200

    Migrate autoloading from psr-0 to psr-4

--- demo/resource.php
+++ demo/resource.php
@@ -1,14 +1,15 @@
 <?php
 
+require_once __DIR__.'/../vendor/autoload.php';
+
+use Gregwar\Image\Image;
+
 $data = 'iVBORw0KGgoAAAANSUhEUgAAABwAAAASCAMAAAB/2U7WAAAABl'
     .'BMVEUAAAD///+l2Z/dAAAASUlEQVR4XqWQUQoAIAxC2/0vXZDr'
     .'EX4IJTRkb7lobNUStXsB0jIXIAMSsQnWlsV+wULF4Avk9fLq2r'
     .'8a5HSE35Q3eO2XP1A1wQkZSgETvDtKdQAAAABJRU5ErkJggg==';
 $data = base64_decode($data);
 
-require_once '../autoload.php';
-
-use Gregwar\Image\Image;
 
 $gd = imagecreatefromstring($data);
 
